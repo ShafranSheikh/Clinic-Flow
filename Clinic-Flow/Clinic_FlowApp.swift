71458ae2c5130e9724afeb4030d0614e0affa47b
ShafranSheikh <shafransheikh@gmail.com>
2026-02-17 21:21:38 +0530

    Initial Commit

---
+++ Clinic-Flow/Clinic_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clinic_FlowApp.swift
+//  Clinic-Flow
+//
+//  Created by Shafran on 2026-02-17.
+//
+
+import SwiftUI
+
+@main
+struct Clinic_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
